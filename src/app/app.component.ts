c4d21f60961d9b3269b597637c19d33eeb264067
Douglas Laiber <douglaslbosco@gmail.com>
2024-04-18 16:08:49 -0300

    chore: configure project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'softplan-pokemon';
+
+  constructor() {
+    setTheme('bs4');
+  }
 }
